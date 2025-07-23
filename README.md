8490b37d172a4fb7e5ac7a1bcff073710fee72ce
G4LXY <angelinatsuboi@proton.me>
2025-07-22 17:22:02 -0700

    feat: more docs

--- README.md
+++ README.md
@@ -7,7 +7,74 @@ conda create --name pyromap
 conda activate pyromap
 ```
 
+### Scope of this Project // include a dashboard to contain all of the following... 
+- Data Analysis and Visualization of the Fire Data 
+- Fire Severity Classification
+- Thermal Signature Fingerprinting
+- Most Dangerous Fires // take into consideration population density, proximity to cities, infrastructure damage, and other factors
+- Predictive Fire Spread Estimator
+
+- Article to Introduce Beginners to the Project // How to use the NASA FIRMS API
+
 ### Libraries 
 - pandas
 
 ### Datasets
+Fire Information for Resource Management System
+
+### Instruments
+- MODIS (Moderate Resolution Imaging Spectroradiometer) // Aqua and Terra
+- VIIRS (Visible Infrared Imaging Radiometer Suite) // Suomi NPP and NOAA-20 and NOAA-21
+
+Available within 3 hrs of satellite observation
+
+### User Interface of FIRMS NASA 
+
+### Dataset Information Retrieval
+
+VIIRS I-Band 375m Active Fire Data
+- provides a greater response over fires of relatively small areas
+- provides improved mapping of large fire perimeters
+- enhanced nighttime performance
+
+### Dataset Distribution Titles 
+https://www.earthdata.nasa.gov/data/instruments/viirs/viirs-i-band-375-m-active-fire-data
+
+- Latitude: lat of center of nominal 375m fire pixel
+- Longitude: lon of center of nominal 375m fire pixel
+- Bright_ti4: VIIRS I-4 channel brightness temperature of the fire pixel measured in Kelvin
+- Scan: Along Scan pixel size... algorithm produces 375 m pixels at the nadir (center) of the scan
+- Track: Along Track pixel size... algorithm produces 375 m pixels at the nadir (center) of the scan
+- Acq_Date: Date of acquisition in UTC
+- Acq_Time: Time of acquisition in UTC
+- Satellite: Satellite platform
+- Confidence: Confidence level of the fire detection.. quality of the pixel
+  - sun glint: low confidence
+  - low rel temp anamoly: low confidence
+- Version: collection and source of data processing
+- Bright_ti5: VIIRS I-5 channel brightness temperature of the fire pixel measured in Kelvin
+- FRP (Fire Radiative Power): Fire Radiative Power in MW
+- Day_Night: Day or Night observation
+
+### Regional Coordinates 
+
+- **World**: West: -180, South: -90, East: 180, North: 90
+- **Canada**: West: -150, South: 40, East: -49, North: 79
+- **Alaska**: West: -180, South: 50, East: -139, North: 72
+- **USA (Conterminous) & Hawaii**: West: -160.5, South: 17.5, East: -63.8, North: 50
+- **Central America**: West: -119.5, South: 7, East: -58.5, North: 33.5
+- **South America**: West: -112, South: -60, East: -26, North: 13
+- **Europe**: West: -26, South: 34, East: 35, North: 82
+- **North and Central Africa**: West: -27, South: -10, East: 52, North: 37.5
+- **Southern Africa**: West: 10, South: -36, East: 58.5, North: -4
+- **Russia and Asia**: West: 26, South: 9, East: 180, North: 83.5
+- **South Asia**: West: 54, South: 5.5, East: 102, North: 40
+- **South East Asia**: West: 88, South: -12, East: 163, North: 31
+- **Australia and New Zealand**: West: 110, South: -55, East: 180, North: -10
+
+# To 
+https://firms.modaps.eosdis.nasa.gov/academy
+- [ ] Data Ingestion and Manipulation
+- [ ] Using FIRMS API
+- [ ] Data Visualization
+- [ ] Data Subsetting
--- data/data_retrieval.py
+++ data/data_retrieval.py
@@ -1,4 +1,126 @@
 import pandas as pd
 
-url = "https://firms.modaps.eosdis.nasa.gov/data/active_fire/c6.1/csv/MODIS_C6_1_Global_24h.csv"
-df = pd.read_csv(url)
+url = "https://firms.modaps.eosdis.nasa.gov/content/notebooks/sample_viirs_snpp_071223.csv"
+
+def get_data(url):
+    """
+    Fetches the data from the specified URL and returns it as a pandas DataFrame.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the data from the CSV file.
+    """
+    try:
+        df = pd.read_csv(url)
+        return df
+    except Exception as e:
+        print(f"An error occurred while fetching the data: {e}")
+        return None
+
+# Fetch the data
+df = get_data(url)
+print ('Rows: %i \nCols: %i' % (df.shape[0], df.shape[1]))
+
+# Show all the column titles
+print("Column Titles:")
+for col in df.columns:
+    print(col)
+
+# Get unique version values
+unique_versions = df['version'].unique()
+print("\nUnique Version Values:")
+for version in unique_versions:
+    print(version)
+
+# Show the length of each unique version column
+print ('Version 2.0NRT has %i records and version 2.0URT has %i records' % (len(df[df['version']=='2.0NRT']), len(df[df['version']=='2.0URT'])))
+
+# Crude Bounding Box Method for Obtaining Fire Data over a Specific Area
+
+# Create a function that allows you to obtain fire data over a specific region using the bounding box method specified by the following URL:
+# https://firms.modaps.eosdis.nasa.gov/active_fire/coordinates.html
+
+regions = {
+    "World": {"West": -180, "South": -90, "East": 180, "North": 90},
+    "Canada": {"West": -150, "South": 40, "East": -49, "North": 79},
+    "Alaska": {"West": -180, "South": 50, "East": -139, "North": 72},
+    "USA (Conterminous) & Hawaii": {"West": -160.5, "South": 17.5, "East": -63.8, "North": 50},
+    "Central America": {"West": -119.5, "South": 7, "East": -58.5, "North": 33.5},
+    "South America": {"West": -112, "South": -60, "East": -26, "North": 13},
+    "Europe": {"West": -26, "South": 34, "East": 35, "North": 82},
+    "North and Central Africa": {"West": -27, "South": -10, "East": 52, "North": 37.5},
+    "Southern Africa": {"West": 10, "South": -36, "East": 58.5, "North": -4},
+    "Russia and Asia": {"West": 26, "South": 9, "East": 180, "North": 83.5},
+    "South Asia": {"West": 54, "South": 5.5, "East": 102, "North": 40},
+    "South East Asia": {"West": 88, "South": -12, "East": 163, "North": 31},
+    "Australia and New Zealand": {"West": 110, "South": -55, "East": 180, "North": -10}
+}
+
+def get_fire_data(region, df):
+    """
+    Filters the DataFrame for fire data within the specified region's bounding box.
+
+    Args:
+        region (str): The name of the region to filter by.
+        df (pd.DataFrame): The DataFrame containing fire data.
+
+    Returns:
+        pd.DataFrame: Filtered DataFrame containing fire data for the specified region.
+    """
+    if region not in regions:
+        print(f"Region '{region}' not found.")
+        return None
+
+    bbox = regions[region]
+    filtered_df = df[(df['longitude'] >= bbox['West']) &
+                     (df['longitude'] <= bbox['East']) &
+                     (df['latitude'] >= bbox['South']) &
+                     (df['latitude'] <= bbox['North'])].copy()
+
+    return filtered_df.reset_index(drop=True) # resetting the index of the dataset
+
+# World
+world_df = get_fire_data("World", df)
+# Canada
+canada_df = get_fire_data("Canada", df)
+# Alaska
+alaska_df = get_fire_data("Alaska", df)
+# USA (Conterminous) & Hawaii
+usa_df = get_fire_data("USA (Conterminous) & Hawaii", df)
+# Central America
+central_america_df = get_fire_data("Central America", df)
+# South America
+south_america_df = get_fire_data("South America", df)
+# Europe
+europe_df = get_fire_data("Europe", df)
+# North and Central Africa
+north_africa_df = get_fire_data("North and Central Africa", df)
+# Southern Africa
+southern_africa_df = get_fire_data("Southern Africa", df)
+# Russia and Asia
+russia_asia_df = get_fire_data("Russia and Asia", df)
+# South Asia
+south_asia_df = get_fire_data("South Asia", df)
+# South East Asia
+southeast_asia_df = get_fire_data("South East Asia", df)
+# Australia and New Zealand
+australia_df = get_fire_data("Australia and New Zealand", df)
+
+region_dfs = {
+    "World": get_fire_data("World", df),
+    "Canada": get_fire_data("Canada", df),
+    "Alaska": get_fire_data("Alaska", df),
+    "USA (Conterminous) & Hawaii": get_fire_data("USA (Conterminous) & Hawaii", df),
+    "Central America": get_fire_data("Central America", df),
+    "South America": get_fire_data("South America", df),
+    "Europe": get_fire_data("Europe", df),
+    "North and Central Africa": get_fire_data("North and Central Africa", df),
+    "Southern Africa": get_fire_data("Southern Africa", df),
+    "Russia and Asia": get_fire_data("Russia and Asia", df),
+    "South Asia": get_fire_data("South Asia", df),
+    "South East Asia": get_fire_data("South East Asia", df),
+    "Australia and New Zealand": get_fire_data("Australia and New Zealand", df)
+}
+
+print("Fire records per region:")
+print(" | ".join([f"{region}: {len(dataframe)}" for region, dataframe in region_dfs.items()]))
+
